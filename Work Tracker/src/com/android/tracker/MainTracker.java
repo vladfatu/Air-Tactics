275624cda0519a30b7774440ad6d1923fda7cef9
vladfatu <colegu123@gmail.com>
2012-01-26 23:40:47 +0200

    -

---
+++ Work Tracker/src/com/android/tracker/MainTracker.java
@@ -0,0 +1,13 @@
+package com.android.tracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainTracker extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
